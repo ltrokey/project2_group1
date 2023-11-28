4e439fa12528ca438d26a5ed8ac21297e49edea7
aubrey johnson <aubreymlj96@gmail.com>
2023-11-28 15:41:49 -0500

    Table notes for models (temporary)

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,26 @@
 DROP DATABASE IF EXISTS coinquest_db;
 CREATE DATABASE coinquest_db;
+
+-- // Table users {
+-- //     id integer [primary key]
+-- //     username varchar
+-- //     password len(8)
+-- //   }
+-- //   Table category {
+-- //     id integer [primary key]
+-- //     name varchar
+-- //     products_id integer [ref: > products.id] //one to many
+-- //   }
+-- //   Table products {
+-- //     id integer [primary key]
+-- //     title varchar
+-- //     price DECIMAL
+-- //     category_id integer [ref: > category.id] //many to one
+-- //   }
+-- //   Table comment {
+-- //     id integer [primary key]
+-- //     title varchar
+-- //     content varchar
+-- //     product_id integer [ref: > products.id] // many to many
+-- //     user_id integer [ref: > users.id] //one to many
+-- //   }
