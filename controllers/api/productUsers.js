73bad5715e0c58aea320c66726105cb26d9c2022
Lacey Trokey <trokeyln@gmail.com>
2023-11-28 20:06:26 -0700

    minor formatting and add routes file for products

--- controllers/api/productUsers.js
+++ controllers/api/productUsers.js
@@ -1,16 +1,3 @@
 const router = require("express").Router();
 
-const { ProductUsers } = require("../../models");
-
-const getAllProductUsers = async () => {
-  try {
-    const productUsers = await ProductUsers.findAll();
-    console.log("Product Users:", productUsers);
-  } catch (error) {
-    console.error("Error fetching product users:", error);
-  }
-};
-
-getAllProductUsers();
-
 module.exports = router;
