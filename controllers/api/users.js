1b91c799a21f087dc5ce2584dadbb6b931c0d147
Lacey Trokey <trokeyln@gmail.com>
2023-11-29 09:30:00 -0700

    added post route to USERS for logout

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -62,4 +62,15 @@ router.post("/login", async (req, res) => {
   }
 });
 
+//Logout - NEED to add button & JS function
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
