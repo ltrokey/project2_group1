522bf38af9dd59ded2521448935425e5b54eed9e
Lacey Trokey <trokeyln@gmail.com>
2023-11-29 08:13:42 -0700

    set up index.js routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
 
+const apiRoutes = require("./api");
+const homeRoutes = require("./homePageRoutes");
+const profileRoutes = require("./profileRoutes");
+
+router.use("/api", apiRoutes);
+route.use("/", homeRoutes);
+router.use("/profile", profileRoutes);
+
 module.exports = router;
