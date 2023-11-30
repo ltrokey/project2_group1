cb523c5387fde1f8c1b723c0289407c64f4b62e2
aubrey johnson <aubreymlj96@gmail.com>
2023-11-29 22:35:36 -0500

    commiting updates to update main branch

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -5,16 +5,8 @@ const {Users, Products} = require('../models');
 router.get('/profile', (req, res) => {
     const userData = {
         username: username,
-        items: [
-            { title: 'Drone', price: 64.00, image: '/public/images/02_cat_upto100_drone.img'},
-            { title: 'Karaoke', price: 37.00, image: '/public/images/02_cat_upto100_karaoke.img'},
-            { title: 'LEGO Disney Princess', price: 52.00, image: '/public/images/02_cat_upto100_legoprincess.img'},
-            { title: 'Hoverboard', price: 90.00, image: '/public/images/02_cat_upto100_hoverboard.img'},
-            { title: 'Laser Tag', price: 120.00, image: '/public/images/02_cat_upto100_lazertag.img'},
-            { title: 'Smart Robot', price: 120.00, image: '/public/images/02_cat_upto100_smartrobot.img'},
-        ]
+        items: items
     };
-
     res.render('profile', userData); 
 });
 
