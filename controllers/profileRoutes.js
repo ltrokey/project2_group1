e5158fee33e332a34ac4d76589ee19180bc61cf3
aubrey johnson <aubreymlj96@gmail.com>
2023-11-29 23:24:03 -0500

    additions to the addComment file as well as updates to the profileRoutes

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -2,12 +2,26 @@ const sequelize = require('../config/connection');
 const router = require("express").Router();
 const {Users, Products} = require('../models');
 
-router.get('/profile', (req, res) => {
-    const userData = {
-        username: username,
-        items: items
-    };
-    res.render('profile', userData); 
+router.get('/', (req, res) => {
+    Users.findAll({
+        attributes: { exclude: ['[password']}
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err); 
+        res.status(500).json(err);
+    });
 });
+
+router.get('/profile', (req, res) => {
+    Products.findAll({
+        attributes: ['title', 'price', 'filename']
+      })
+    })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 
 module.exports = router;
