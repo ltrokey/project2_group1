e165f439d730a42c15b74000653fa1c9ef038aa9
Lacey Trokey <trokeyln@gmail.com>
2023-11-30 19:36:42 -0700

    fixed favorites button

--- controllers/api/productUsers.js
+++ controllers/api/productUsers.js
@@ -13,42 +13,36 @@ router.post("/:productId", withAuth, async (req, res) => {
     }
 
     // Create a ProductUser record
-    const productUser = await ProductUsers.create({
-      products_id: productId,
-      users_id: userId,
-    });
-
-    res.status(201).json(productUser);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "An unexpected error occurred." });
-  }
-});
-
-//DELETE Distroy ProductUsers by Id
-router.delete("/:productId", withAuth, async (req, res) => {
-  try {
-    const { productId } = req.params;
-
-    const userId = req.session.users_id;
-    if (!userId) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-
-    await ProductUsers.destroy({
+    const userHasProduct = await ProductUsers.count({
       where: {
         products_id: productId,
         users_id: userId,
       },
     });
 
-    res.status(204).end();
+    if (userHasProduct === 0) {
+      const productUser = await ProductUsers.create({
+        products_id: productId,
+        users_id: userId,
+      });
+
+      return res.status(201).json(productUser);
+    } else {
+      await ProductUsers.destroy({
+        where: {
+          products_id: productId,
+          users_id: userId,
+        },
+      });
+
+      return res.status(204).end();
+    }
+
+    // Create a ProductUser record
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "An unexpected error occurred." });
   }
 });
-
-module.exports = router;
 
 module.exports = router;
--- public/js/favoriteItem.js
+++ public/js/favoriteItem.js
@@ -9,9 +9,17 @@ async function addToFavorites() {
       },
     });
 
+    console.log("favorite", response.ok);
     if (response.ok) {
-      console.log("Product added to favorites!");
-      alert("Product added to your Quest Items!");
+      if (response.status == 204) {
+        console.log("Quest item removed!");
+        alert("Quest item removed!");
+        window.location.href = "/profile";
+      } else if (response.status == 201) {
+        console.log("Quest item added!");
+        alert("Quest item added!");
+        window.location.href = "/";
+      }
     } else {
       console.error("Failed to add product to favorites");
       alert("Failed to add product to favorites. Please try again.");
@@ -28,38 +36,4 @@ document
     event.preventDefault();
 
     addToFavorites();
-  });
-
-async function removeFromFavorites() {
-  try {
-    const productId = window.location.pathname.split("/").pop();
-
-    const response = await fetch(`/api/productUsers/${productId}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      console.log("Product removed from favorites!");
-      alert("Product removed from your Quest Items!");
-
-      window.location.href = "/profile";
-    } else {
-      console.error("Failed to remove product from favorites");
-      alert("Failed to remove product from favorites. Please try again.");
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    alert("An unexpected error occurred. Please try again later.");
-  }
-}
-
-document
-  .getElementById("favorite-item")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-
-    removeFromFavorites();
   });
